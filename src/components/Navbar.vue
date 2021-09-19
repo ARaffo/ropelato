0b185565d459826f4d0f1d1993fef7ade8099d53
ARaffo <49041262+ARaffo@users.noreply.github.com>
2021-09-19 18:01:44 -0300

    New pantallas ropelato

---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="Navbar">
+         <v-app-bar color="black" fixed app>
+            <v-toolbar-title class="white--text">Menu</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <div>
+         
+            </div>
+        </v-app-bar>    
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+
+    //components: { ComponentName },
+
+    //directives: { DirectiveName },
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
