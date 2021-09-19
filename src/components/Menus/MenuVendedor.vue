b7380554c6497abd751f46c4667497c4794cfd32
ARaffo <49041262+ARaffo@users.noreply.github.com>
2021-09-19 18:01:55 -0300

    menu

---
+++ src/components/Menus/MenuVendedor.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RopelatoMenuvendedor',
+
+    
+
+    directives: { DirectiveName },
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
